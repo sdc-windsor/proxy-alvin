b2c1b67027799d82397c97f59934206dcf1566e7
akwong372 <akwong372@gmail.com>
2019-04-27 13:25:43 -0700

    Add loader io endpoint

--- server/app.js
+++ server/app.js
@@ -4,4 +4,16 @@ const app = express();
 app.use(express.static('public'));
 app.use('/:id', express.static('public'));
 
+
+//endpoint for loader.io validation
+app.get('/:file.txt', (req, res, next) => {
+  res.sendfile(`${req.params.file}.txt`, { root: __dirname }, (err) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log('file sent');
+    }
+  })
+});
+
 module.exports = app;
